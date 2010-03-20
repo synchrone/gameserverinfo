af2908446049346abfa088c583dff0d81606eb93
Aleksandr Bogdanov <syn@syn.im>
2010-03-20 07:18:56 +0000

    git-svn-id: http://gameserverinfo.googlecode.com/svn/trunk@2 7a407be1-71ad-1668-5652-0db8fcf8ea7f

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameServerInfo")]
+[assembly: AssemblyDescription("A game server query library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("digital-wave")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+[assembly: AssemblyVersion("1.0.1.0")]
+
+[assembly:ComVisible(false)]
+[assembly:CLSCompliant(true)]
+
+[assembly: AssemblyDelaySign(false)]
